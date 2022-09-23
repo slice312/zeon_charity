18c6647ebf9944058dd80cd849ca6f3eab2f7023
Talgat <tairovtalgat@gmail.com>
2022-09-23 14:57:26 +0600

    add api for adverts

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,12 @@
 import {Router} from "./Router.jsx"
 import "./styles/index.scss";
 import Header from "./header/header";
+import {useEffect} from "react";
 
 
 export const App = () => {
+    console.log("APP", import.meta.env.VITE_WEB_API_URL);
+
     return (
         <div className="App">
           <Header/>
