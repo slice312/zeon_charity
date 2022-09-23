edebc70f6a994576651c4a64ae1489cd3011fce9
Estebes <estebesm95@gmail.com>
2022-09-23 08:15:48 +0600

    feature/added_home_page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,8 +3,11 @@ import MainCarousel from "../../components/mainCarousel/mainCarousel.jsx"
 
 export const Home = () => {
     return (
-        <div className={css.carousel__container}>
-            <MainCarousel/>
+        // <div className={css.carousel__container}>
+        //     <MainCarousel/>
+        // </div>
+        <div className={'home'}>
+
         </div>
     )
 }
